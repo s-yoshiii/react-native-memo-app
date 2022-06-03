ff8e47c5dd914611c33d8d16b528cb292f8923bd
sa_yoshiii <sayako.yoshikawa1129@gmail.com>
2022-06-03 21:04:37 +0900

    コンポーネントの作成

--- src/components/Hello.jsx
+++ src/components/Hello.jsx
@@ -1,12 +1,20 @@
 import React from 'react';
 import { View, Text, StyleSheet } from 'react-native';
 
-const Hello = () => {
+function Hello() {
   return (
     <View>
-      <Text>Hello</Text>
+      <Text style={styles.text}>Hello</Text>
     </View>
   );
-};
-
+}
+const styles = StyleSheet.create({
+  text: {
+    color: '#fff',
+    backgroundColor: 'blue',
+    fontSize: 40,
+    fontWeight: 'bold',
+    padding: 16,
+  },
+});
 export default Hello;
