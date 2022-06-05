51ffd8c8da6f9e5c90df646647838335aa546839
sa_yoshiii <sayako.yoshikawa1129@gmail.com>
2022-06-06 00:15:11 +0900

    メモ一覧をマークアップ

--- App.jsx
+++ App.jsx
@@ -1,18 +1,44 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
-import Hello from './src/components/Hello';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Hello bang>World</Hello>
-      <Hello bang style={{ fontSize: 16 }}>
-        Small World
-      </Hello>
-      <Text>Open up App.js to start working on your app!</Text>
-      {/* eslint-disable-next-line */}
-      <StatusBar style='auto' />
+      <View>
+        <Text>Memo App</Text>
+        <Text>ログアウト</Text>
+      </View>
+      <View>
+        <View>
+          <View>買い物リスト</View>
+          <View>2022/06/06 0:00:00</View>
+        </View>
+        <View>
+          <Text>✕</Text>
+        </View>
+      </View>
+      <View>
+        <View>
+          <View>買い物リスト</View>
+          <View>2022/06/06 0:00:00</View>
+        </View>
+        <View>
+          <Text>✕</Text>
+        </View>
+      </View>
+      <View>
+        <View>
+          <View>買い物リスト</View>
+          <View>2022/06/06 0:00:00</View>
+        </View>
+        <View>
+          <Text>✕</Text>
+        </View>
+      </View>
+      <View>
+        <Text>+</Text>
+      </View>
     </View>
   );
 }
