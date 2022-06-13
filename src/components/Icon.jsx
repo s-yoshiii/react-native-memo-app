2818f92403cd851e1986e6fde32856edddea59ca
sa_yoshiii <sayako.yoshikawa1129@gmail.com>
2022-06-13 09:53:27 +0900

    カスタムアイコンを作成する（useFonts)導入

--- src/components/Icon.jsx
+++ src/components/Icon.jsx
@@ -1,11 +1,18 @@
 import React from 'react';
 import { createIconSetFromIcoMoon } from '@expo/vector-icons';
+import { useFonts } from '@use-expo/font';
+
 import icomoon from '../../assets/fonts/icomoon.ttf';
 import selection from '../../assets/fonts/selection.json';
 
-function Icon() {
+function Icon(props) {
+  const [fontloaded] = useFonts({ icomoon });
+  const { name, size, color } = props;
   const CustomIcon = createIconSetFromIcoMoon(selection);
-  return <CustomIcon name="plus" />;
+  if (!fontloaded) {
+    return null;
+  }
+  return <CustomIcon name={name} size={size} color={color} />;
 }
 
 export default Icon;
