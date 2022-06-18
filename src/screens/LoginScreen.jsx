397fad3cbfeac82457ccef6f9145ae7ba9634a12
sa_yoshiii <sayako.yoshikawa1129@gmail.com>
2022-06-18 21:44:09 +0900

    ログイン画面のスタイリング

--- src/screens/LoginScreen.jsx
+++ src/screens/LoginScreen.jsx
@@ -9,11 +9,14 @@ function LoginScreen() {
       <Header />
       <View style={styles.inner}>
         <Text style={styles.title}>Login</Text>
-        <TextInput value="Email Adress" />
-        <TextInput value="Password" />
-        <View>
-          <Text>Not registered?</Text>
-          <Text>Sign up here</Text>
+        <TextInput style={styles.input} value="Email Adress" />
+        <TextInput style={styles.input} value="Password" />
+        <View style={styles.buttonContainer}>
+          <Text style={styles.buttonLabel}>Submit</Text>
+        </View>
+        <View style={styles.footer}>
+          <Text style={styles.footerText}>Not registered?</Text>
+          <Text style={styles.footerLink}>Sign up here</Text>
         </View>
       </View>
     </View>
@@ -32,6 +35,42 @@ const styles = StyleSheet.create({
     fontSize: 24,
     lineHeight: 32,
     fontWeight: 'bold',
+    marginBottom: 24,
+  },
+  input: {
+    backgroundColor: '#fff',
+    fontSize: 16,
+    minHeight: 48,
+    borderColor: '#ddd',
+    borderWidth: 1,
+    paddingHorizontal: 8,
+    marginBottom: 16,
+  },
+  buttonContainer: {
+    backgroundColor: '#876445',
+    borderRadius: 4,
+    alignSelf: 'flex-start',
+    marginBottom: 24,
+  },
+  buttonLabel: {
+    fontSize: 16,
+    lineHeight: 32,
+    paddingVertical: 8,
+    paddingHorizontal: 32,
+    color: '#fff',
+  },
+  footer: {
+    flexDirection: 'row',
+  },
+  footerText: {
+    fontSize: 14,
+    lineHeight: 24,
+    marginRight: 8,
+  },
+  footerLink: {
+    fontSize: 14,
+    lineHeight: 24,
+    color: '#876445',
   },
 });
 
