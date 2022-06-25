09140dfcfe3176ea06141d1c017da64b1b4bdcc8
sa_yoshiii <sayako.yoshikawa1129@gmail.com>
2022-06-25 23:56:59 +0900

    ログアウト機能を実装する

---
+++ src/components/LogOutButton.jsx
@@ -0,0 +1,39 @@
+import React from 'react';
+import { Alert, StyleSheet, Text, TouchableOpacity } from 'react-native';
+import firebase from 'firebase';
+import { useNavigation } from '@react-navigation/native';
+
+function LogOutButton() {
+  const navigation = useNavigation();
+  const handlePress = () => {
+    firebase
+      .auth()
+      .signOut()
+      .then(() => {
+        navigation.reset({
+          index: 0,
+          routes: [{ name: 'Login' }],
+        });
+      })
+      .catch(() => {
+        Alert.alert('ログアウトに失敗しました');
+      });
+  };
+  return (
+    <TouchableOpacity style={styles.container} onPress={handlePress}>
+      <Text style={styles.label}>ログアウト</Text>
+    </TouchableOpacity>
+  );
+}
+const styles = StyleSheet.create({
+  container: {
+    paddingHorizontal: 12,
+    paddingVertical: 4,
+  },
+  label: {
+    fontSize: 14,
+    color: '#fff',
+  },
+});
+
+export default LogOutButton;
--- src/screens/MemoListScreen.jsx
+++ src/screens/MemoListScreen.jsx
@@ -1,10 +1,17 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { View, StyleSheet } from 'react-native';
 import CircleButton from '../components/CircleButton';
+import LogOutButton from '../components/LogOutButton';
 import MemoList from '../components/MemoList';
 
 function MemoListScreen(props) {
   const { navigation } = props;
+  useEffect(() => {
+    navigation.setOptions({
+      // eslint-disable-next-line react/no-unstable-nested-components
+      headerRight: () => <LogOutButton />,
+    });
+  }, []);
   return (
     <View style={styles.container}>
       <MemoList />
